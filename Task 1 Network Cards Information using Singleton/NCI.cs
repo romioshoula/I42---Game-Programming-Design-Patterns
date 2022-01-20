506f931d06856f400a7455ec73b85ece1c8614dd
romioshoula <romioshoula@gmail.com>
2022-01-20 11:07:52 +0200

    Task 1 Network Cards Information using Singleton

---
+++ Task 1 Network Cards Information using Singleton/NCI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace GameDesignPatterns_Task1
+{
+    class NCI
+    {
+        string _manufactor;
+        long _MACadress;
+        public CardType _cardType;
+        private static NCI card = null;
+
+        public NCI(string m, long mac, CardType CardType)
+        {
+            _manufactor = m;
+            _MACadress = mac;
+            _cardType = CardType;
+        }
+
+        public static NCI GetNCI(string manufactor, long MAC, CardType cardtype)
+        {
+            if (card == null)
+            {
+                card = new NCI(manufactor, MAC, cardtype);
+            }
+            else
+            {
+                card._manufactor = manufactor;
+                card._MACadress = MAC;
+                card._cardType = cardtype;
+            }
+            return card;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"The Card Information Is: {_manufactor} --- {_MACadress} --- {_cardType}");
+        }
+
+    }
+
+    public enum CardType
+    {
+        Ethernet = 1,
+        TokenRinf = 2
+    }
+}
---
+++ Task 1 Network Cards Information using Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace GameDesignPatterns_Task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+         
+         var macAddr =
+        (
+        from nic in NetworkInterface.GetAllNetworkInterfaces()
+        where nic.OperationalStatus == OperationalStatus.Up
+        select nic.GetPhysicalAddress().ToString()
+         ).FirstOrDefault();        
+         long ayhaga =   Convert.ToInt64(macAddr,16);
+
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface adapter in interfaces)
+            {
+                if (adapter.Name.ToLower() == "ethernet" || adapter.Name.ToLower() == "wi-fi")
+                {
+                    string initial = adapter.Description;
+                    string output = initial.Substring(0, initial.IndexOf("("));
+
+                    CardType cardt;
+                    if ((Enum.TryParse<CardType>(adapter.Name, out cardt)))
+                    {
+                        var x = NCI.GetNCI(output, ayhaga, cardt);
+                        Console.WriteLine(x.ToString());
+                    }
+                }
+            } 
+            Console.ReadLine();
+        }
+    }
+}
